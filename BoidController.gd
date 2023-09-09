3db1315d12272756d5979e377f2e4722e31c6bac
Skalimoi <skalimoi@protonmail.com>
2023-09-09 12:47:24 +0200

    Added application icon & splash screen
    It looks neat.

--- BoidController.gd
+++ BoidController.gd
@@ -7,34 +7,37 @@ var boids_vel : PackedVector3Array
 var boids_pos : PackedVector3Array
 var boid_number = 100
 var pipeline : RID
+var vel_output : Array
 
 var material_physics = PhysicsMaterial.new()
-var shape = CollisionShape3D.new()
-var sph = CSGSphere3D.new()
+
 var mat = Material.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pipeline = rd.compute_pipeline_create(shader)
-	shape.shape = SphereShape3D.new()
-	shape.shape.set_radius(10.0)
-	sph.set_material(mat)
-	sph.set_radius(10)
+	
+	# boid creation
 	for i in boid_number:
+		var shape = CollisionShape3D.new()
+		var sph = CSGSphere3D.new()
+		shape.shape = SphereShape3D.new()
+		shape.shape.set_radius(10.0)
+		sph.set_material(mat)
+		sph.set_radius(10)
 		var rb = RigidBody3D.new()
 		$"../BoidBase".add_child(rb)
-		await rb.tree_entered
+		# await rb.tree_entered
 		rb.physics_material_override = material_physics
 		rb.gravity_scale = 0
 		rb.add_child(shape)
-		await shape.tree_entered
+		# await shape.tree_entered
 		rb.add_child(sph)
-		await sph.tree_entered
-		var pos = Vector3(randf_range(10.0, 400.0), randf_range(10.0, 400.0), randf_range(10.0, 400.0))
-		print("CREATING BOID NUMBER %d", [i])
-
-		rb.global_position = pos
+		# await sph.tree_entered
+		var pos = Vector3(randf_range(10.0, 1500.0), randf_range(10.0, 1500.0), randf_range(10.0, 1500.0))
 		
+		print("CREATING BOID NUMBER ", [i])
+		rb.global_position = pos
 		
 		
 		boids_pos.append(rb.global_position)
@@ -42,16 +45,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	await $"../BoidBase".ready
 	var vel_byte_array = boids_vel.to_byte_array()
 	var pos_byte_array = boids_pos.to_byte_array()
-	print("VEL BYTE ARRAY DEBUG: %d", [vel_byte_array[0]])
 	var vel_storage_buffer = rd.storage_buffer_create(vel_byte_array.size(), vel_byte_array)
 	var pos_storage_buffer = rd.storage_buffer_create(pos_byte_array.size(), pos_byte_array)
 	var num_storage_buffer = rd.storage_buffer_create([boid_number].size(), [boid_number])
-	# 0 velocity
-	# 1 position
-	# 2 boid number
+	
 	var u_vel = RDUniform.new()
 	u_vel.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
 	u_vel.add_id(vel_storage_buffer)
@@ -74,14 +73,16 @@ func _process(delta):
 	var compute_list = rd.compute_list_begin()
 	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
 	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
-	rd.compute_list_dispatch(compute_list, 2, 1, 1)
+	rd.compute_list_dispatch(compute_list, 10, 1, 1)
 	rd.compute_list_end()
 	rd.submit()
 	rd.sync()
 	
-	var vel_data = rd.buffer_get_data(vel_storage_buffer)
+	var vel_data = rd.buffer_get_data(vel_storage_buffer).to_float32_array()
 	var i = 0
 	for boid in $"../BoidBase".get_children():
 		# check if indexes okay
-		boid.set_linear_velocity(Vector3(vel_data[i * 3],vel_data[i * 3 + 1], vel_data[i * 3 + 2]))
+		boid.linear_velocity = (Vector3(vel_data[i * 3],vel_data[i * 3 + 1], vel_data[i * 3 + 2]))
 		i += 1
+#	for value in vel_data:
+#		print(value)
--- boids/boid_compute.glsl
+++ boids/boid_compute.glsl
@@ -1,8 +1,7 @@
-// empty shader file
 #[compute]
 #version 450
 
-layout(local_size_x = 2, local_size_y = 1, local_size_z = 1) in;
+layout(local_size_x = 10, local_size_y = 1, local_size_z = 1) in;
 
 layout(set = 0, binding = 0, std430) restrict buffer velocity_buffer {
     vec3 data[];
@@ -21,17 +20,19 @@ BoidsNumber;
 
 void main() {
 
-	
+    // We calculate the steering factor for every behavior and then add them to the velocity of the boid.
     // align
-    float distance_limit_1 = 500.0;
+
+    float distance_limit = 1000.0;
+    vec3 pos = PositionBuffer.data[gl_GlobalInvocationID.x];
+    
     vec3 steering_1 = vec3(0.0,0.0,0.0);
     int total_1 = 0;
-    vec3 pos_1 = PositionBuffer.data[gl_GlobalInvocationID.x];
 
     for (int b = 0; b < BoidsNumber.current_boids; b++) {
          if (b != gl_GlobalInvocationID.x) {
-        float distance_1 = distance(pos_1, PositionBuffer.data[b]);
-        if (distance_1 < distance_limit_1) {
+        float distance_1 = distance(pos, PositionBuffer.data[b]);
+        if (distance_1 < distance_limit) {
             steering_1 += VelocityBuffer.data[b];
             total_1 +=1;
         }
@@ -41,16 +42,15 @@ void main() {
         }
       }
     }
+
     // separate
-    float distance_limit_2 = 100.0;
     vec3 steering_2 = vec3(0.0,0.0,0.0);
     int total_2 = 0;
-    vec3 pos_2 = PositionBuffer.data[gl_GlobalInvocationID.x];
 
     for (int b = 0; b < BoidsNumber.current_boids; b++) {
          if (b != gl_GlobalInvocationID.x) {
-        float distance_2 = distance(pos_2, PositionBuffer.data[b]);
-        if (distance_2 < distance_limit_2) {
+        float distance_2 = distance(pos, PositionBuffer.data[b]);
+        if (distance_2 < distance_limit) {
             vec3 difference = PositionBuffer.data[gl_GlobalInvocationID.x] - PositionBuffer.data[b];
             difference /= distance_2 * distance_2;
             steering_2 += difference;
@@ -64,15 +64,13 @@ void main() {
       }
     }
     //cohesion
-    float distance_limit_3 = 500.0;
     vec3 steering_3 = vec3(0.0,0.0,0.0);
     int total_3 = 0;
-    vec3 pos_3 = PositionBuffer.data[gl_GlobalInvocationID.x];
 
     for (int b = 0; b < BoidsNumber.current_boids; b++) {
          if (b != gl_GlobalInvocationID.x) {
-        float distance_3 = distance(pos_3, PositionBuffer.data[b]);
-        if (distance_3 < distance_limit_2) {
+        float distance_3 = distance(pos, PositionBuffer.data[b]);
+        if (distance_3 < distance_limit) {
             steering_3 += PositionBuffer.data[b];
             total_3 +=1;
         }
@@ -83,6 +81,8 @@ void main() {
         }
       }
     }
+
     vec3 new = VelocityBuffer.data[gl_GlobalInvocationID.x] + steering_1 + steering_2 + steering_3;
+    
     VelocityBuffer.data[gl_GlobalInvocationID.x] = new;
 }
